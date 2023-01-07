9a6765e825e32604c977cf68b03d37eeeeeffef4
roasmarialaura <roa20003@byui.edu>
2023-01-07 20:25:24 -0300

    Completed preparation 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter number:");
+            number = int.Parse(Console.ReadLine());
+
+            if (number!= 0)
+            {
+                numbers.Add(number);
+            }
+            
+
+        }
+        //Core requeriment 1
+        float sum = 0;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine($"The sum is {sum}");
+        //Core requeriment 2
+        float average = sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Core requeriment 3
+        int maximun = numbers[0];
+         foreach (int num in numbers)
+        {
+            if (num > maximun)
+            {
+                maximun = num;
+            }
+        }
+        Console.WriteLine($"The maximun number is: {maximun}");
+
+        
     }
 }
